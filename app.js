6377202dc3617f13d8376c2cc5d027110a4607b2
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-09-05 08:40:25 +0300

    init

---
+++ app.js
@@ -0,0 +1,56 @@
+async function readTag() {
+  // if ('NDEFReader' in window) {
+  const ndef = new NDEFReader();
+
+  try {
+    await ndef.scan();
+    ndef.onreading = (event) => {
+      const decoder = new TextDecoder();
+      for (const record of event.message.records) {
+        alert('Record type: ' + record.recordType);
+        alert('MIME type: ' + record.mediaType);
+        alert('=== data ===\n' + decoder.decode(record.data));
+      }
+    };
+  } catch (error) {
+    alert(error);
+  }
+  // } else {
+  // alert('Web NFC is not supported.');
+  // }
+}
+
+async function writeTag() {
+  // if ('NDEFReader' in window) {
+  try {
+    await ndef.write('Аптека №911');
+    alert('write success');
+    await readTag();
+  } catch {
+    alert('Write failed');
+  }
+  // } else {
+  // alert('Web NFC is not supported.');
+  // }
+}
+
+const readButton = document.querySelector('#read');
+
+readButton.addEventListener('click', async () => {
+  if ('NDEFReader' in window) {
+    const ndef = new NDEFReader();
+    try {
+      await ndef.scan();
+      ndef.onreading = (event) => {
+        const decoder = new TextDecoder();
+        for (const record of event.message.records) {
+          alert('Record type: ' + record.recordType);
+          alert('MIME type: ' + record.mediaType);
+          alert('=== data ===\n' + decoder.decode(record.data));
+        }
+      };
+    } catch (error) {
+      alert(error);
+    }
+  }
+});
