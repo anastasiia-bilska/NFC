5dd45bd501bf73f6ff8e4955793f540299631f79
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-09-07 07:31:07 +0300

    update

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-async function readTag() {
-  // if ('NDEFReader' in window) {
-  const ndef = new NDEFReader();
+alert(window.NDEFReader);
 
-  try {
-    await ndef.scan();
-    ndef.onreading = (event) => {
-      const decoder = new TextDecoder();
-      for (const record of event.message.records) {
-        alert('Record type: ' + record.recordType);
-        alert('MIME type: ' + record.mediaType);
-        alert('=== data ===\n' + decoder.decode(record.data));
-      }
-    };
-  } catch (error) {
-    alert(error);
-  }
-  // } else {
-  // alert('Web NFC is not supported.');
-  // }
-}
+// async function readTag() {
+//   if ('NDEFReader' in window) {
+//     const ndef = new NDEFReader();
 
-async function writeTag() {
-  // if ('NDEFReader' in window) {
-  try {
-    await ndef.write('Аптека №911');
-    alert('write success');
-    await readTag();
-  } catch {
-    alert('Write failed');
-  }
-  // } else {
-  // alert('Web NFC is not supported.');
-  // }
-}
+//     try {
+//       await ndef.scan();
+//       ndef.onreading = (event) => {
+//         const decoder = new TextDecoder();
+//         for (const record of event.message.records) {
+//           alert('Record type: ' + record.recordType);
+//           alert('MIME type: ' + record.mediaType);
+//           alert('=== data ===\n' + decoder.decode(record.data));
+//         }
+//       };
+//     } catch (error) {
+//       alert(error);
+//     }
+//   } else {
+//     alert('Web NFC is not supported.');
+//   }
+// }
+
+// async function writeTag() {
+//   if ('NDEFReader' in window) {
+//   try {
+//     await ndef.write('Аптека №911');
+//     alert('write success');
+//     await readTag();
+//   } catch {
+//     alert('Write failed');
+//   }
+//   } else {
+//   alert('Web NFC is not supported.');
+//   }
+// }
 
 const readButton = document.querySelector('#read');
+
+const writeButton = document.querySelector('write');
 
 readButton.addEventListener('click', async () => {
   if ('NDEFReader' in window) {
@@ -55,4 +59,17 @@ readButton.addEventListener('click', async () => {
   } else {
     alert('Web NFC is not supported.');
   }
+});
+writeButton.addEventListener('click', async () => {
+    if ('NDEFReader' in window) {
+    try {
+      await ndef.write('Аптека №911');
+      alert('write success');
+      await readTag();
+    } catch {
+      alert('Write failed');
+    }
+    } else {
+    alert('Web NFC is not supported.');
+    }
 });
